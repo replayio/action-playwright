b2adf0108761a95d0fd1ad378bd4e6e5f8d2323c
Ryan Duffy <ryan@replay.io>
2022-03-15 11:15:43 -0700

    Initial commit

---
+++ upload.js
@@ -0,0 +1,22 @@
+const cli = require("@recordreplay/recordings-cli")
+
+async function uploadFailedRecordings() {
+  const recordings = cli
+    .listAllRecordings()
+    .filter((r) => r.metadata.testStatus === 'failed');
+
+  const results = await Promise.allSettled(recordings.map(r => cli.uploadRecording(r.id, {verbose: true})));
+
+  results.forEach(r => {
+    if (r.status === "rejected") {
+      console.error("Failed to upload replay:", r.reason);
+    }
+  });
+  
+  return cli
+    .listAllRecordings()
+    .filter((r) => r.status === 'uploaded')
+    .map(r => ({id: r.recordingId, title: r.metadata.title}));
+}
+
+module.exports = uploadFailedRecordings;
