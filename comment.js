ae99b83d22c7b95de57a4528a6fed8f3b34407f7
Ryan Duffy <ryan@replay.io>
2022-06-17 12:49:16 -0700

    Add test run link to comment

--- comment.js
+++ comment.js
@@ -1,4 +1,48 @@
-function comment({github, context, issue_number, recordings, uploadAll, source}) {
+async function getWorkspaceId(apiKey) {
+	const resp = await fetch("https://api.replay.io/v1/graphql", {
+		method: "POST",
+		headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+		body: JSON.stringify({
+      variables: {},
+      query: `
+        query GetWS {
+          viewer {
+            user {
+              id
+            }
+          }
+          auth {
+            workspaces {
+              edges {
+                node {
+                  id
+                }
+              }
+            }
+          }
+        }
+      `,
+    }),
+	});
+  
+  const json = await resp.json();
+
+  if (json.errors || !json.data || json.data.user) {
+    return null;
+  }
+
+  const workspaces = json.data.auth.workspaces.edges;
+  if (workspaces.length !== 1) {
+    return null;
+  }
+
+  return workspaces[0].id;
+}
+
+async function comment({apiKey,  github, context, issue_number, recordings, uploadAll, source, testRunId}) {
   const {
     repo: {owner, repo},
   } = context;
@@ -17,13 +61,25 @@ function comment({github, context, issue_number, recordings, uploadAll, source})
   const upload = uploadAll ? '' : recordings.length === 1 ? ' of a failed test' : ' of failed tests';
   const sourceText = source ? ` from **${source}**` : '';
 
+  let testRunMessage = "";
+  if (apiKey && testRunId) {
+    const workspaceId = await getWorkspaceId(apiKey);
+    if (workspaceId) {
+      testRunMessage = `View the [entire test run](https://app.replay.io/team/${workspaceId}/test-run/${testRunId}) on Replay.`;
+    }
+  }
+
   return github.rest.issues.createComment({
     issue_number,
     owner,
     repo,
     body: `# [![logo](https://static.replay.io/images/logo-horizontal-small-light.svg)](https://app.replay.io)
 
-:wave: Hey there! We uploaded ${count}${upload}${sourceText}.
+:wave: Hey there!
+
+${testRunMessage}
+
+We uploaded ${count}${upload}${sourceText}.
 
 ${recordings
   .map(
